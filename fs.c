3504c71351dcd764f120b4faffaeba064acb4891
michaelxuuu <michaelxu@gmail.com>
2024-04-23 18:39:17 -0700

    added doubly-indirect blocks, got rid of the in-memory inode structure since we are not implementing inode cache

--- fs.c
+++ fs.c
@@ -3,6 +3,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <assert.h>
+#include <string.h>
 
 struct {
     int vd; // file desc pointing to the "virtual disk"
@@ -19,6 +20,12 @@ static void block_write(int n, char buf[BLOCKSIZE]) {
     assert(lseek(fs.vd, n * BLOCKSIZE, SEEK_SET) == n * BLOCKSIZE);
     assert(read(fs.vd, buf, BLOCKSIZE) == BLOCKSIZE);
 }
+// Zero a block
+static void block_zero(int n) {
+    char buf[BLOCKSIZE] = {0};
+    block_write(n, buf);
+}
+
 
 // Load super block from disk
 static void load_super(struct superblock *su) {
@@ -67,69 +74,157 @@ static int bitmap_free(u32 n) {
 
 // inode operations
 // Load the inode with the inode number 'n' into memory
-static int inode_load(int n, struct inode *p) {
+static int inode_load(int n, struct dinode *p) {
     union dblock b;
     struct superblock su;
     load_super(&su);
     if (n >= su.ninodes)
         return -1;
-    block_read(su.sinode + n/NINODES_PERBLOCK, &b);
-    p->num = n;
-    p->din = b.inodes[n%NINODES_PERBLOCK];
+    block_read(su.sinode + n/NINODES_PER_BLOCK, &b);
+    *p = b.inodes[n%NINODES_PER_BLOCK];
     return 0;
 }
 
-// Update the on-disk inode with the inode number 'p->inum' with p->din
-static int inode_save(struct inode *p) {
+// Update the on-disk inode with the inode number 'n'
+static int inode_save(int n, struct dinode *p) {
     union dblock b;
     struct superblock su;
     load_super(&su);
-    if (p->num >= su.ninodes)
+    if (n >= su.ninodes)
         return -1;
-    block_read(su.sinode + p->num/NINODES_PERBLOCK, &b);
-    b.inodes[p->num%NINODES_PERBLOCK] = p->din;
-    block_write(su.sinode + p->num/NINODES_PERBLOCK, &b);
+    block_read(su.sinode + n/NINODES_PER_BLOCK, &b);
+    b.inodes[n%NINODES_PER_BLOCK] = *p;
+    block_write(su.sinode + n/NINODES_PER_BLOCK, &b);
     return 0;
 }
 
-// Free an inode. Need also to free all data blocks linked
-// in-memory inode is passed in, but only 'p->num' is used
-// - the freeing is based on the on-disk inode. It must be
-// made sure the on-disk inode is update-to-date!
-static int inode_free(struct inode *p) {
-    union dblock b;
-    struct superblock su;
-    load_super(&su);
-    if (p->num >= su.ninodes || p->num == 1) // can't free the root directory inode
-        return -1;
-    block_read(su.sinode + p->num/NINODES_PERBLOCK, &b);
-    struct dinode *di = &b.inodes[p->num%NINODES_PERBLOCK]; // grab a pointer to it
-    di->type = 0; // free the inode itself
-    // free data blocks
-    for (int i = 0; i < NDIRECT+NINDRECT; i++) {
-        if (!di->addrs[i])
-            continue;
+// // Free an inode. Need also to free all data blocks linked
+// static int inode_free(u32 n) {
+//     union dblock b;
+//     struct superblock su;
+//     load_super(&su);
+//     if (n >= su.ninodes || n == 1) // can't free the root directory inode
+//         return -1;
+//     block_read(su.sinode + n/NINODES_PER_BLOCK, &b);
+//     struct dinode *di = &b.inodes[n%NINODES_PER_BLOCK]; // grab a pointer to it
+//     di->type = 0; // free the inode itself
+//     // free data blocks
+//     for (int i = 0; i < NPTRS_CLASS3; i++) {
+//         if (!di->ptrs[i])
+//             continue;
             
-        if (i < NDIRECT)
-            assert(!bitmap_free(di->addrs[i]));
-        else {
-            // read the direct block
-            union dblock ib;
-            block_read(di->addrs[i], &ib);
-            // free data blocks
-            for (int j = 0; j < NADDRS_PERBLOCK; j++)
-                if (ib.addrs[i])
-                    assert(!bitmap_free(di->addrs[j]));
-            // free the indirect block
-            assert(!bitmap_free(di->addrs[i]));
+//         if (i < NDIRECT)
+//             assert(!bitmap_free(di->ptrs[i]));
+//         else {
+//             // read the direct block
+//             union dblock ib;
+//             block_read(di->ptrs[i], &ib);
+//             // free data blocks
+//             for (int j = 0; j < NPTRS_PER_BLOCK; j++)
+//                 if (ib.addrs[i])
+//                     assert(!bitmap_free(di->ptrs[j]));
+//             // free the indirect block
+//             assert(!bitmap_free(di->ptrs[i]));
+//         }
+//     }
+//     // write the inode back to disk
+//     block_write(su.sinode + n/NINODES_PER_BLOCK, &b);
+//     return 0;
+// }
+
+// Sweeb through the inode blocks and return the inum of the free inode if found.
+static int inode_alloc(u16 type) {
+    struct superblock su;
+    if (type > T_DEV)
+        return -1;
+    load_super(&su);
+    for (int i = 0; i < su.nblock_inode; i++) {
+        union dblock b;
+        block_read(i + su.sinode, &b);
+        for (int j = 0; j < NINODES_PER_BLOCK; j++) {
+            if (!b.inodes[j].type) {
+                struct dinode *p = &b.inodes[j];
+                memset(p, 0, sizeof(*p));
+                p->type = type;
+                p->nlink = 1;
+                block_write(i + su.sinode, &b);
+                return 0;
+            }
         }
     }
-    // write the inode back to disk
-    block_write(su.sinode + p->num/NINODES_PERBLOCK, &b);
+    return -1;
+}
+
+static int inode_write(u32 n, char buf[], u32 len, u32 off) {
+    struct superblock su;
+    load_super(&su);
+    struct dinode di;
+    if (inode_load(n, &di))
+        return -1;
+    u32 end = off + len;
+    if (end > di.size) di.size = end;
+    u32 sb = off/BLOCKSIZE;
+    u32 eb = end/BLOCKSIZE;
+    // check if there's enough disk space left for us to write
+    for (int i = sb; i < eb; i++) {
+        u32 sz;
+        int tmp;
+        u32 start;
+        u32 *direct;
+        u32 *indirect;
+        u32 *dindirect;
+        union dblock b; // data block
+        union dblock ib; // doubly indirect block (if used)
+        union dblock dib; // indirect block (if used)
+
+        direct = 0;
+        indirect = 0;
+        dindirect = 0;
+
+        sz = (i == sb) ? (BLOCKSIZE - off % BLOCKSIZE) : ((BLOCKSIZE < len) ? BLOCKSIZE : len);
+        start = (i == sb) ? off % BLOCKSIZE : 0;
+
+        len -= sz;
+        buf += sz;
+
+        if (i >= NBLOCKS_TO_DINDIRECT)
+            return -1;
+
+        if (i >= NBLOCKS_TO_INDIRECT)
+            dindirect = &di.ptrs[(i - NBLOCKS_TO_INDIRECT) / NBLOCKS_BY_DINDRECT];
+        if (dindirect && ((i - NBLOCKS_TO_INDIRECT) % NBLOCKS_BY_DINDRECT) == 0)
+                block_read(*dindirect, &dib);
+
+        if (dindirect)
+            indirect = &dib.ptrs[(i - (tmp = NBLOCKS_TO_INDIRECT)) / NBLOCKS_BY_INDRECT];
+        else if (i >= NBLOCKS_TO_DIRECT)
+            indirect = &di.ptrs[(i - (tmp = NBLOCKS_TO_DIRECT)) / NBLOCKS_BY_INDRECT];
+        if (indirect && ((i - tmp) % NBLOCKS_BY_INDRECT) == 0)
+            block_read(*indirect, &ib);
+
+        if (indirect)
+            direct = ib.ptrs[(i - NBLOCKS_TO_DIRECT) % NBLOCKS_BY_INDRECT];
+        else
+            direct = di.ptrs[i];
+    
+        block_read(*direct, &b);
+        memcpy(&b.bytes[start], buf, len);
+        block_write(*direct, &b);
+
+        // write the doubly indirect block and indirect blocks back if they're used (regardless of whether they've been modified or not)
+        if (dindirect && ((i - NBLOCKS_TO_INDIRECT) % NBLOCKS_BY_DINDRECT) == (NBLOCKS_BY_DINDRECT - 1))
+            block_write(*dindirect, &dib);
+        if (indirect && ((i - tmp) % NBLOCKS_BY_INDRECT) == (NBLOCKS_BY_INDRECT - 1))
+            block_write(*indirect, &ib);
+    }
+
+    // write the inode back
+    inode_save(n, &di);
     return 0;
 }
 
-static int inode_alloc(struct inode *p) {
+
+static int inode_read(u32 n, char b[], u32 l, u32 o) {
 
 }
 
--- fs.h
+++ fs.h
@@ -18,8 +18,31 @@ struct superblock {
     u32 sdata;
 };
 
-#define NDIRECT 12
-#define NINDRECT 1
+#define NINODES_PER_BLOCK       (BLOCKSIZE/sizeof(struct dinode))
+#define NDIRENTS_PER_BLOCK      (BLOCKSIZE/sizeof(struct dirent))
+#define NPTRS_PER_BLOCK         (BLOCKSIZE/sizeof(u32))
+
+// #direct, indirect, and doubly-indirect, and total pointers in an inode
+#define NDIRECT                 10
+#define NINDRECT                2
+#define NDINDRECT               1
+#define NPTRS                   (NDIRECT+NINDRECT+NDINDRECT)
+
+// #blocks pointed to by an inode indirect pointer
+#define NBLOCKS_BY_INDRECT      NPTRS_PER_BLOCK
+// #blocks pointed to by an inode doubly-indirect pointer
+#define NBLOCKS_BY_DINDRECT     (NPTRS_PER_BLOCK*NPTRS_PER_BLOCK)
+
+// #blocks pointed to by all direct pointers in an inode
+#define NBLOCKS_TO_DIRECT       NDIRECT
+// #blocks pointed to by all direct and indirect pointers in an inode
+#define NBLOCKS_TO_INDIRECT     (NBLOCKS_TO_DIRECT+NINDRECT*NPTRS_PER_BLOCK)
+// #blocks pointed to by all direct, indirect and doubly-indirect pointers in an indoe
+#define NBLOCKS_TO_DINDIRECT    (NBLOCKS_TO_INDIRECT+NDINDRECT*NPTRS_PER_BLOCK*NPTRS_PER_BLOCK)
+
+#define T_REG 1
+#define T_DIR 2
+#define T_DEV 3
 // On-disk inode sturcture
 struct dinode {
   u16 type;
@@ -27,13 +50,7 @@ struct dinode {
   u16 minor;
   u16 nlink;
   u32 size;
-  u32 addrs[NDIRECT+NINDRECT];
-};
-
-// in-memory copy of the on-disk inode
-struct inode {
-    int num;
-    struct dinode din;
+  u32 ptrs[NDIRECT+NINDRECT];
 };
 
 // Directory entry sturcture
@@ -48,15 +65,11 @@ struct dirent {
     char name[MAX_FILE_NAME];
 };
 
-#define NINODES_PERBLOCK (BLOCKSIZE / sizeof(struct dinode))
-#define NDIRENTS_PERBLOCK (BLOCKSIZE / sizeof(struct dirent))
-#define NADDRS_PERBLOCK (BLOCKSIZE / sizeof(u32))
-
 union dblock {
     struct superblock su;
     u8  bytes[BLOCKSIZE];
-    u32 addrs[NADDRS_PERBLOCK];
-    struct dinode inodes[NINODES_PERBLOCK];
-    struct dirent dirents[NDIRENTS_PERBLOCK];
+    u32 ptrs[NPTRS_PER_BLOCK];
+    struct dinode inodes[NINODES_PER_BLOCK];
+    struct dirent dirents[NDIRENTS_PER_BLOCK];
 };
 
