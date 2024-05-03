78a7fef4e790c10fe4cc7cd4004d445955f837f3
michaelxuuu <michaelxu@gmail.com>
2024-05-02 17:12:44 -0700

    combined inode_write and inode_read into inode_rw, and combined recursive_write and recursive_read into recursive_rw. The code gets much shorter, but is it still a good idea when think of addding multithreading and caching support where the behavior of read and write can vary a lot there? Would this change cost us performance related optimization opportunities?

--- testprog.c
+++ testprog.c
@@ -1,23 +1,31 @@
 #include "fs.h"
 
 #include <stdio.h>
+#include <string.h>
+
+#define TEST_BSIZE 128
+#define TEST_TEXT "Lily wakes up..."
+#define TEST_TEXT_SZ strlen(TEST_TEXT)
+#define TEST_FNAME "lily"
 
 int main(int argc, char *argv[]) 
 {
     fs_init(argv[1]);
-    u32 src = 0xdeadbeef;
-    u32 dest = 0;
+    char b1[TEST_BSIZE] = TEST_TEXT;
+    char b2[TEST_BSIZE] = {0};
     // create a file
     u32 i0 = alloc_inode(T_REG);
-    for (int i = 0; i < 1; i++)
-        inode_write(i0, &src, 4, 0);
-    // link the file to root dir
-    struct dirent i0file = {
+    // write b1 to the file
+    inode_write(i0, &b1, TEST_TEXT_SZ, 0);
+    // put the file under root dir
+    struct dirent de = {
         .inum = i0,
-        .name = "i0file",
+        .name = TEST_FNAME,
     };
-    inode_write(ROOTINUM, &i0file, sizeof(i0file), 0);
-    u32 i1 = fs_lookup("/i0file");
-    inode_read(i1, &dest, 4, 0);
-    printf("%x\n", dest);
+    inode_write(ROOTINUM, &de, sizeof(de), 0);
+    // look up the file
+    u32 i1 = fs_lookup("/" TEST_FNAME);
+    // read from the file to see if it gives us the original text
+    inode_read(i1, &b2, TEST_TEXT_SZ, 0);
+    printf("%s\n", b2);
 }
