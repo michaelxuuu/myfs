8cf6aa72744392ecec9de243cf6f4af0d0ad1c22
michaelxuuu <xyjmichael@gmail.com>
2024-05-08 00:55:08 -0700

    moved sparse test to the bash script where it should've been in the first place :)

--- autotest.sh
+++ autotest.sh
@@ -1,4 +1,4 @@
-RED='\033[1;32m'
+RED='\033[1;31m'
 GREEN='\033[1;32m'
 RESET='\033[0m'
 
@@ -7,17 +7,80 @@ pass() {
 }
 
 fail() {
-    echo "${GREEN}failed: $1${RESET}"
+    echo "${RED}failed: $1${RESET}"
 }
 
-touch input.tmp
-echo "migrate /fs.c fs.c" >> input.tmp
-echo "retrieve fs.c.tmp /fs.c" >> input.tmp
-echo "quit" >> input.tmp
-./main vhd < input.tmp
-if [ -n $(diff fs.c.tmp fs.c) ]; then
-    pass "two files match"
-else
-    fail "two files differ"
-fi
-rm *.tmp
+cleanup() {
+    rm *.tmp
+}
+
+doecho() {
+    for cmd in "$@"; do
+        echo "$cmd"
+    done
+}
+
+matchline() {
+    line=$(sed -n "${1}p" $3)
+    if [ $line = $2 ]; then
+        echo "1"
+    fi
+}
+
+migrate_test() {
+    touch input.tmp
+    commands=(
+        "migrate /fs.c fs.c"
+        "retrieve fs.c.tmp /fs.c"
+        "quit"
+    )
+    doecho "${commands[@]}" > input.tmp
+    ./main vhd < input.tmp
+    if [ -n $(diff fs.c.tmp fs.c) ]; then
+        pass "two files match"
+    else
+        fail "two files differ"
+    fi
+}
+
+NDIRECT=10
+NINDIRECT=2
+NDINDIRECT=1
+BSZ=512
+sparse_test() {
+    touch input.tmp > out.tmp
+    commands=(
+        "touch /sparse"
+        "write /sparse 0 1 a"
+        "write /sparse $((NDIRECT*BSZ)) 1 a"
+        "write /sparse $((NDIRECT*BSZ+NINDIRECT*BSZ*BSZ)) 1 a"
+        "read /sparse $((BSZ)) 1"
+        "read /sparse $((NDIRECT*BSZ+BSZ)) 1"
+        "read /sparse $((NDIRECT*BSZ+NINDIRECT*BSZ*BSZ)) 1"
+        "stat /sparse"
+        "quit"
+    )
+    doecho "${commands[@]}" > input.tmp
+    ./main vhd < input.tmp > out.tmp
+    size=$(awk -F: '$1 == "size" { print $2 }' out.tmp)
+    # Check if the size is 100
+    if [ "$size" -eq $((NDIRECT*BSZ+NINDIRECT*BSZ*BSZ+1)) ]; then
+        pass "sparse: size is $((NDIRECT*BSZ+NINDIRECT*BSZ*BSZ+1))"
+    else
+        fail "sparse: size is not $((NDIRECT*BSZ+NINDIRECT*BSZ*BSZ+1))"
+    fi
+
+    # Check if the lines match the expected content
+    if [ "$(matchline 1 "\0" out.tmp)" ] && \
+        [ "$(matchline 2 "\0" out.tmp)" ] && \
+        [ "$(matchline 3 "a" out.tmp)" ]; then
+        pass "sparse: read result matches the expected outcome"
+    else
+        fail "sparse: read result differs from the expected outcome"
+    fi
+}
+
+migrate_test
+cleanup
+sparse_test
+cleanup
--- main.c
+++ main.c
@@ -15,6 +15,7 @@
 #include <time.h>
 #include <fcntl.h>
 #include <stdio.h>
+#include <ctype.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
@@ -131,38 +132,59 @@ static void cmd_retrieve(char *hostpath, char *mypath) {
     assert(myfs_close(myfd) >= 0);
 }
 
-static void cmd_sparse(char *path) {
-    int fd;
+static void cmd_touch(char *path) {
     if (myfs_mknod(path, T_REG)) {
-        fprintf(stderr, "failed to create %s in myfs\n", path);
+        fprintf(stderr, "myfs_mknod failed\n");
         return;
     }
-    assert((fd = myfs_open(path, O_RDWR)) >= 0);
-    char c = '@';
-    // write to the first data block
-    assert(myfs_write(fd, &c, 1) == 1);
-    // write to the first data block pointed by the first singly-indirect block
-    assert(myfs_seek(fd, NDIRECT*BLOCKSIZE) >= 0);
-    assert(myfs_write(fd, &c, 1) == 1);
-    // write to the first data block pointed by the first doubly-indirect block
-    assert(myfs_seek(fd, NDIRECT*BLOCKSIZE+NINDRECT*BLOCKSIZE*BLOCKSIZE) >= 0);
-    assert(myfs_write(fd, &c, 1) == 1);
-    // check file size
+}
+
+static void cmd_stat(char *path) {
+    int fd;
+    if ((fd = myfs_open(path, O_WRONLY)) < 0) {
+        fprintf(stderr, "myfs_open failed\n");
+        return;
+    }
     struct filestat st;
     assert(myfs_stat(fd, &st) >= 0);
-    assert(st.size == NDIRECT*BLOCKSIZE+NINDRECT*BLOCKSIZE*BLOCKSIZE+1);
-    // some random reads from the ranges that are not allocated data blocks but valid
-    assert(myfs_seek(fd, BLOCKSIZE) >= 0);
-    assert(myfs_read(fd, &c, 1) == 1);
-    assert(c == '\0');
-    assert(myfs_seek(fd, NDIRECT*BLOCKSIZE+BLOCKSIZE) >= 0);
-    assert(myfs_read(fd, &c, 1) == 1);
-    assert(c == '\0');
-    // this read falls into an allocated data block
-    assert(myfs_seek(fd, NDIRECT*BLOCKSIZE+NINDRECT*BLOCKSIZE*BLOCKSIZE) >= 0);
-    assert(myfs_read(fd, &c, 1) == 1);
-    assert(c == '@');
-    myfs_close(fd);
+    printf("type:%d\nsize:%d\nlinkcnt:%d\n", st.type, st.size, st.linkcnt);
+}
+
+static void cmd_write(char *path, u32 off, u32 sz, char *words) {
+    int fd;
+    if ((fd = myfs_open(path, O_WRONLY)) < 0) {
+        fprintf(stderr, "myfs_open failed\n");
+        return;
+    }
+    assert(myfs_seek(fd, off) >= 0);
+    assert(myfs_write(fd, words, strlen(words)) == strlen(words));
+    assert(myfs_close(fd) >= 0);
+}
+
+static void cmd_read(char *path, u32 off, u32 sz) {
+    int fd;
+    if ((fd = myfs_open(path, O_RDONLY)) < 0) {
+        fprintf(stderr, "myfs_open failed\n");
+        return;
+    }
+    char buf[BLOCKSIZE];
+    assert(myfs_seek(fd, off) >= 0);
+    int n = myfs_read(fd, buf, sz);
+    if (n < 0) {
+        printf("myfs_read failed\n");
+        assert(myfs_close(fd) >= 0);
+        return;
+    }
+    for (int i = 0; i < n; i++) {
+        if (isprint(buf[i]))
+            printf("%c", buf[i]);
+        else if (!buf[i])
+            printf("\\0");
+        else
+            printf("\\?");
+    }
+    puts("");
+    assert(myfs_close(fd) >= 0);
 }
 
 #define CMDLEN 32
@@ -175,11 +197,11 @@ int main(int argc, char *argv[])
     fs_init(argv[1]);
     for (;;) {
         char cmd[CMDLEN];
-        printf("> "), fflush(stdout);
+        // printf("> "), fflush(stdout);
         fgets(cmd, CMDLEN, stdin);
         cmd[strlen(cmd) - 1] = 0;
-        char *args[3];
-        int cnt = parse_args(cmd, args, 3);
+        char *args[5];
+        int cnt = parse_args(cmd, args, 5);
         if (!cnt)
             continue;
         if (!strncmp(args[0], "ls", 2)) {
@@ -202,11 +224,26 @@ int main(int argc, char *argv[])
                 fprintf(stdout, "usage: retrieve <host_path> <myfs_path>\n");
             else
                 cmd_retrieve(args[1], args[2]);
-        } else if (!strncmp(args[0], "sparse", 6)) {
-            if (cnt < 2)
-                fprintf(stdout, "usage: sparse <path>\n");
+        } if (!strncmp(args[0], "read", 4)) {
+            if (cnt < 4)
+                fprintf(stdout, "read: read <path> <off> <size>\n");
             else
-                cmd_sparse(args[1]);
+                cmd_read(args[1], atoi(args[2]), atoi(args[3]));
+        } if (!strncmp(args[0], "write", 5)) {
+            if (cnt < 5)
+                fprintf(stdout, "write: write <path> <off> <size> <words>\n");
+            else
+                cmd_write(args[1], atoi(args[2]), atoi(args[3]), args[4]);
+        } if (!strncmp(args[0], "stat", 4)) {
+            if (cnt < 2)
+                fprintf(stdout, "stat: stat <path>\n");
+            else
+                cmd_stat(args[1]);
+        } if (!strncmp(args[0], "touch", 5)) {
+            if (cnt < 2)
+                fprintf(stdout, "touch: touch <path>\n");
+            else
+                cmd_touch(args[1]);
         } else if (!strncmp(args[0], "quit", 4))
             exit(0);
     }
